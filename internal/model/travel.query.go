60e2aa99e1ddc114f057549fe1f7543f7765da18
ardimr <rizkyardimaulana@gmail.com>
2023-07-08 10:25:07 +0700

    Modified Travel Schedule model

--- internal/model/travel.query.go
+++ internal/model/travel.query.go
@@ -18,15 +18,31 @@ type Travel struct {
 }
 
 type TravelSchedule struct {
-	TravelID           int64     `json:"travel_id,omitempty"`
-	TravelCode         int64     `json:"travel_code"`
-	TrainCode          string    `json:"train_code"`
-	TrainName          string    `json:"train_name"`
-	TrainClass         string    `json:"train_class"`
-	DepartureStation   string    `json:"departure_station"`
-	DestinationStation string    `json:"destination_station"`
-	DepartureSchedule  time.Time `json:"departure_schedule"`
-	ArrivalSchedule    time.Time `json:"arrival_schedule"`
-	Duration           string    `json:"duration"`
-	Cost               int       `json:"cost"`
+	TravelID           int64      `json:"travel_id,omitempty"`
+	TravelCode         int64      `json:"travel_code"`
+	TrainCode          string     `json:"train_code"`
+	TrainName          string     `json:"train_name"`
+	WagonClass         WagonClass `json:"wagon_class"`
+	DepartureStation   Station    `json:"departure_station"`
+	DestinationStation Station    `json:"destination_station"`
+	DepartureSchedule  time.Time  `json:"departure_schedule"`
+	ArrivalSchedule    time.Time  `json:"arrival_schedule"`
+	Duration           Duration   `json:"duration"`
+	Cost               Cost       `json:"cost"`
+	AvailableSeats     int        `json:"available_seats"`
+}
+
+type WagonClass struct {
+	Code string `json:"wagon_class_code"`
+	Name string `json:"wagon_class_name"`
+}
+
+type Duration struct {
+	Hour   int `json:"duration_hour"`
+	Minute int `json:"duration_minute"`
+}
+
+type Cost struct {
+	Currency string `json:"currency"`
+	Amount   int    `json:"amount"`
 }
