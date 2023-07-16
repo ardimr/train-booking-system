8b99333a3f9e04bb870325abcbe0e8c558d7c5dd
ardimr <rizkyardimaulana@gmail.com>
2023-07-16 21:50:23 +0700

    Updated count available seats apporach

--- internal/repository/travel.repository.go
+++ internal/repository/travel.repository.go
@@ -26,9 +26,10 @@ func (q *PostgresRepository) ListTravels(ctx context.Context, param model.Travel
 	travel_tickets AS (
 		SELECT
 			travel_id,
-			seat_id
+			passengers.seat_id
 		FROM
 			travel_schedules.tickets
+			INNER JOIN users.passengers ON passengers.ticket_id = tickets.ticket_id
 	),
 	num_available_seats AS (
 		SELECT
