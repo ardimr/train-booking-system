260b6810a7eeee5b3ab3896c2c4368715f8faa2e
ardimr <rizkyardimaulana@gmail.com>
2023-07-16 21:49:29 +0700

    Added user model

---
+++ internal/model/user.request.go
@@ -0,0 +1,22 @@
+package model
+
+type NewUserRequestBody struct {
+	FullName    string      `json:"full_name" binding:"required"`
+	Username    string      `json:"username" binding:"required"`
+	Password    string      `json:"password" binding:"required"`
+	Email       string      `json:"email" binding:"required,email"`
+	PhoneNumber PhoneNumber `json:"phone_number" binding:"required"`
+	IdType      string      `json:"id_type" binding:"required"`
+	IdNumber    string      `json:"id_number" binding:"required"`
+	BornDate    string      `json:"born_date" binding:"required"`
+}
+
+type PhoneNumber struct {
+	CountryCode string `json:"country_code"`
+	Number      string `json:"number"`
+}
+
+type Identity struct {
+	Type   string `json:"type"`
+	Number string `json:"number"`
+}
