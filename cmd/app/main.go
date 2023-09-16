c8b2a4c845db7e2f5782a4eda62d5fae7223b4ca
ardimr <rizkyardimaulana@gmail.com>
2023-09-16 12:47:56 +0700

    Run with seat domain and station domain

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -11,6 +11,18 @@ import (
 	"github.com/ardimr/train-booking-system/internal/repository"
 	router "github.com/ardimr/train-booking-system/internal/routes"
 
+	// Seats
+	seat "github.com/ardimr/train-booking-system/internal/seat"
+	seatController "github.com/ardimr/train-booking-system/internal/seat/controller"
+	seatRepository "github.com/ardimr/train-booking-system/internal/seat/repository"
+	seatUseCase "github.com/ardimr/train-booking-system/internal/seat/usecase"
+
+	// Stations
+	station "github.com/ardimr/train-booking-system/internal/station"
+	stationController "github.com/ardimr/train-booking-system/internal/station/controller"
+	stationRepository "github.com/ardimr/train-booking-system/internal/station/repository"
+	stationUseCase "github.com/ardimr/train-booking-system/internal/station/usecase"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/gzip"
 
@@ -88,6 +100,20 @@ func main() {
 	corsConfig := cors.Default()
 	restServer.Use(corsConfig)
 
+	// Seat Router
+	seatRepo := seatRepository.NewSeatRepository(dbConnection)
+	seatRedisRepo := seatRepository.NewSeatRedisRepository(redisClient)
+	seatUseCase := seatUseCase.NewSeatUseCase(seatRepo, seatRedisRepo)
+	seatController := seatController.NewSeatController(seatUseCase)
+	seatRouter := seat.NewSeatRouter(seatController)
+	seatRouter.RegisterRoute(restServer.Group("/api"))
+
+	// Station Router
+	stationRepo := stationRepository.NewStationRepository(dbConnection)
+	stationUseCase := stationUseCase.NewStationUsecase(stationRepo)
+	stationController := stationController.NewStationController(stationUseCase)
+	stationRouter := station.NewStationRouter(stationController)
+	stationRouter.RegisterRoute(restServer.Group("/api"))
 	// Setup Router
 	tbsController := controller.NewController(repository.NewPostgresRepository(dbConnection), repository.NewRedisRepository(redisClient))
 	tbsRouter := router.NewRouter(tbsController)
