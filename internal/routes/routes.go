7f13b08d3067b60f865aae149769c0d562e98b7e
ardimr <rizkyardimaulana@gmail.com>
2023-09-16 15:50:56 +0700

    Comment travel, booking, and ticket routes

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -17,23 +17,23 @@ func NewRouter(controller *controller.Controller) *Router {
 }
 
 func (router *Router) AddRoute(superRoute *gin.RouterGroup) {
-	router.travelRoutes(superRoute)
+	// router.travelRoutes(superRoute)
 	// router.stationRoutes(superRoute)
 	// router.seatRoutes(superRoute)
-	router.ticketRoutes(superRoute)
-	router.bookingRoutes(superRoute)
+	// router.ticketRoutes(superRoute)
+	// router.bookingRoutes(superRoute)
 	router.userRoutes(superRoute)
 	router.wagonRoutes(superRoute)
 }
 
-func (router *Router) travelRoutes(superRoute *gin.RouterGroup) {
-	travelRouter := superRoute.Group("/v1")
-	travelRouter.GET("/travels", router.controller.ListTravels)
-	travelRouter.GET("/travels/:id", router.controller.GetTravelById)
-	travelRouter.POST("/travels", router.controller.AddNewTravel)
-	travelRouter.PATCH("/travels/:id", router.controller.UpdateTravelById)
-	travelRouter.DELETE("/travels/:id", router.controller.DeleteTravel)
-}
+// func (router *Router) travelRoutes(superRoute *gin.RouterGroup) {
+// 	travelRouter := superRoute.Group("/v1")
+// 	travelRouter.GET("/travels", router.controller.ListTravels)
+// 	travelRouter.GET("/travels/:id", router.controller.GetTravelById)
+// 	travelRouter.POST("/travels", router.controller.AddNewTravel)
+// 	travelRouter.PATCH("/travels/:id", router.controller.UpdateTravelById)
+// 	travelRouter.DELETE("/travels/:id", router.controller.DeleteTravel)
+// }
 
 // func (router *Router) stationRoutes(superRoute *gin.RouterGroup) {
 // 	stationRouter := superRoute.Group("/v1")
@@ -61,14 +61,14 @@ func (router *Router) userRoutes(superRoute *gin.RouterGroup) {
 	userRouter.GET("/users/:user_id", router.controller.GetUserDetails)
 }
 
-func (router *Router) bookingRoutes(superRoute *gin.RouterGroup) {
-	bookingRouter := superRoute.Group(("/v1"))
-	bookingRouter.POST("/bookings", router.controller.NewBooking)
-	bookingRouter.GET("/bookings/:booking_code", router.controller.GetBookingDetails)
-}
+// func (router *Router) bookingRoutes(superRoute *gin.RouterGroup) {
+// 	bookingRouter := superRoute.Group(("/v1"))
+// 	bookingRouter.POST("/bookings", router.controller.NewBooking)
+// 	bookingRouter.GET("/bookings/:booking_code", router.controller.GetBookingDetails)
+// }
 
-func (router *Router) ticketRoutes(superRoute *gin.RouterGroup) {
-	ticketRouter := superRoute.Group("/v1")
-	ticketRouter.GET("/tickets", router.controller.GetUserTickets)
-	ticketRouter.GET("/tickets/:ticket_id", router.controller.GetTicketDetailsById)
-}
+// func (router *Router) ticketRoutes(superRoute *gin.RouterGroup) {
+// 	ticketRouter := superRoute.Group("/v1")
+// 	ticketRouter.GET("/tickets", router.controller.GetUserTickets)
+// 	ticketRouter.GET("/tickets/:ticket_id", router.controller.GetTicketDetailsById)
+// }
