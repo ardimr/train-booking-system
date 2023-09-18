5c8ac959fa4f25f13cfa0cd85c8054bf2b748a21
ardimr <rizkyardimaulana@gmail.com>
2023-09-18 22:14:55 +0700

    Created HttpError

--- internal/exception/httpError.go
+++ internal/exception/httpError.go
@@ -1,1 +1,37 @@
 package exception
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+type HttpError struct {
+	Description string `json:"description,omitempty"`
+	StatusCode  int    `json:"statusCode"`
+}
+
+func NewHttpError(statusCode int, description string) HttpError {
+	return HttpError{
+		Description: description,
+		StatusCode:  statusCode,
+	}
+}
+
+func ParseError(err error) HttpError {
+	switch {
+	case errors.Is(err, sql.ErrNoRows):
+		return NewHttpError(http.StatusNotFound, "Not Found")
+	default:
+		return NewHttpError(http.StatusInternalServerError, "Internal Server Error")
+	}
+}
+
+func (e HttpError) Error() string {
+	return fmt.Sprintf("description: %s", e.Description)
+}
+
+func (e HttpError) Status() int {
+	return e.StatusCode
+}
