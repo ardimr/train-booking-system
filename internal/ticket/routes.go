9e42c12440cbb4dc8a1b253ae4502e87c6653438
ardimr <rizkyardimaulana@gmail.com>
2023-10-15 22:12:57 +0700

    Added auth service

--- internal/ticket/routes.go
+++ internal/ticket/routes.go
@@ -1,22 +1,26 @@
-package seat
+package ticket
 
 import (
+	"github.com/ardimr/train-booking-system/internal/auth"
+	"github.com/ardimr/train-booking-system/internal/middleware"
 	"github.com/ardimr/train-booking-system/internal/ticket/controller"
 	"github.com/gin-gonic/gin"
 )
 
 type TicketRouter struct {
 	ticketController *controller.TicketController
+	auth             *auth.AuthService
 }
 
-func NewTicketRouter(ticketController *controller.TicketController) *TicketRouter {
+func NewTicketRouter(ticketController *controller.TicketController, auth *auth.AuthService) *TicketRouter {
 	return &TicketRouter{
 		ticketController: ticketController,
+		auth:             auth,
 	}
 }
 
 func (router *TicketRouter) RegisterRoute(superRoute *gin.RouterGroup) {
 	ticketRouter := superRoute.Group("/v1")
-	ticketRouter.GET("/tickets", router.ticketController.GetUserTickets)
-	ticketRouter.GET("/tickets/:ticket_id", router.ticketController.GetTicketDetailsById)
+	ticketRouter.GET("/tickets", middleware.ValidateToken(router.auth), router.ticketController.GetUserTickets)
+	ticketRouter.GET("/tickets/:ticket_id", middleware.ValidateToken(router.auth), router.ticketController.GetTicketDetailsById)
 }
