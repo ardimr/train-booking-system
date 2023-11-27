89272db9c5e74fbe2355040619856895f0242d44
ardimr <rizkyardimaulana@gmail.com>
2023-11-28 05:26:32 +0700

    Removed seats checking in redis

--- internal/seat/usecase/usecase.go
+++ internal/seat/usecase/usecase.go
@@ -32,26 +32,7 @@ func (uc *SeatUseCase) FindAvailableSeats(ctx context.Context, param model.Avail
 	}
 
 	if len(availableSeats) == 0 {
-		return nil, errors.New("no available seat")
-	}
-
-	// Check in redis for any existing booked seats
-	bookedSeats, err := uc.seatRedisRepo.GetBookedSeats(ctx, param.TravelId)
-	if err != nil {
-		return nil, err
-	}
-
-	for _, bookedSeat := range bookedSeats {
-		// Overide the avaiability status
-		for wagonIdx, wagon := range availableSeats {
-			for rowIdx, row := range wagon.SeatingRows {
-				for seatIdx, seat := range row.RowElements {
-					if bookedSeat == seat.SeatId {
-						availableSeats[wagonIdx].SeatingRows[rowIdx].RowElements[seatIdx].Available = false
-					}
-				}
-			}
-		}
+		return nil, errors.New("no available travel.seats")
 	}
 
 	return availableSeats, nil
